5581264d338e7a362e5737bcf98da449fad95ee8
Levan Asanoghli <asanoghli@outlook.com>
2025-02-25 21:19:42 +0400

    JS - next step compelted

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 'use strict';
 
 const multiStepInfo = {
-    name: '',
-    email: '',
-    phone: '',
-    location: '',
-    homeType: '',
-    budgetId: -1
-}
-const ValidationRules = {
-    MIN_NAME_LENGTH: 2,
-    MIN_EMAIL_LENGTH: 5,
-    EMAIL_REGEX: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    PHONE_NUMBER_FORMAT_REGEX: /^5\d{8}$/,
-    LOCATION_MIN_LENGTH: 3
+    name: '', email: '', phone: '', location: '', homeType: '', budgetId: -1
 }
 const elements = {
-    details: {
+    contactDetailsForm: {
         name: document.getElementById('name'),
         name_error: document.getElementsByClassName('name-error')[0],
 
@@ -61,58 +49,129 @@ const elements = {
     last_step: document.getElementById('last-step'),
     budget_radio_buttons: document.querySelectorAll('.budget-radio-button'),
 }
-const steps = [
-    {
+const steps = [{
 
-        element: elements.details.mainContent,
-        progressBar: elements.details.progressBar,
-    },
-    {
-        element: elements.home_details_elements.mainContent,
-        progressBar: elements.home_details_elements.progressBar
-    },
-    {
-        element: elements.budget_details_elements.mainContent,
-        progressBar: elements.budget_details_elements.progressBar
-    },
-    {
-        element: elements.last_step_elements.mainContent,
-        progressBar: elements.last_step_elements.progressBar,
-    }
-]
+    element: elements.contactDetailsForm.mainContent, progressBar: elements.contactDetailsForm.progressBar,
+}, {
+    element: elements.home_details_elements.mainContent, progressBar: elements.home_details_elements.progressBar
+}, {
+    element: elements.budget_details_elements.mainContent, progressBar: elements.budget_details_elements.progressBar
+}, {
+    element: elements.last_step_elements.mainContent, progressBar: elements.last_step_elements.progressBar,
+}]
 let currentStepIndex = 0;
-const IsFirstStepValid = function (){
-    let data = {name : elements.details.name.value, email : elements.details.email.value, phone : elements.details.phone.value, location : elements.details.location.value};
-    let isValid =  Boolean(data.name.length && data.email.length && data.location.length && data.phone.length);
+const ValidationRulesForContactDetailsMap = new Map([
+    ['name', {
+        required: true,
+        fieldName: 'name',
+        hasMinLength: true,
+        minLength: 2,
+        errorMessage: 'Name is required'
+    }],
+    ['email', {
+        required: true,
+        fieldName: 'email',
+        hasMinLength: true,
+        minLength: 5,
+        isEmail: true,
+        errorMessage: 'Email is required'
+    }],
+    ['phone', {
+        required: true,
+        fieldName: 'phone',
+        isPhone: true,
+        hasMinLength: true,
+        minLength: 9,
+        errorMessage: 'Phone is required'
+    }],
+    ['location', {
+        required: true, fieldName: 'location', hasMinLength: true, minLength: 2, errorMessage: 'Name is required'
+    }],])
 
-    return {isValid,data};
-}
-const ShowErrors = function (showErrors){
-    if(!showErrors){
-        Array.from(document.getElementsByClassName('error-message')).forEach((el) => {
-            el.classList.remove('hidden');
-        })
-        return;
+const Validator = function (validationRules, value) {
+    let response = {errorMessage: '', isValid: false};
+    if (validationRules.required) {
+        response.errorMessage = `${validationRules.fieldName} must not be empty.`;
+        response.isValid = value !== null && value !== undefined && value.length > 0;
+    }
+    if (!response.isValid) return response;
+
+    if (validationRules.hasMinLength) {
+        response.isValid = value.length >= validationRules.minLength;
+        response.errorMessage = `${validationRules.fieldName} must be min. ${validationRules.minLength} length`;
+    }
+    if (validationRules.isEmail) {
+        response.isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        response.errorMessage = `${value} is not a valid email address.`;
+    }
+    if (validationRules.isPhone) {
+        response.isValid = /^5\d{8}$/.test(value);
+        response.errorMessage = `${value} is not a valid phone number.`;
     }
 
-    let allKeys = Object.keys(multiStepInfo);
-    for(let i = 0; i < allKeys.length; i++){
-        let errorInputElementSpan = document.querySelector(`.${allKeys[i]}-error`);
-
-        if(multiStepInfo[allKeys[i]].length){ // if value is NOT -  NULL or NAN or Undefined or ''
-            if(errorInputElementSpan)
-            errorInputElementSpan.classList.add('hidden');
-        }
-        else{
-            if(errorInputElementSpan)
-            errorInputElementSpan.classList.remove('hidden');
-        }
-
-    }
+    return response;
 }
+const ValidateCurrentForm = function () {
+    let isAllInputsValid = [];
+    switch (currentStepIndex) {
+        case 0: // Validate Contact Details Form
+            isAllInputsValid = [];
+            let FieldValidatorsKeys = ValidationRulesForContactDetailsMap.keys();
 
+            FieldValidatorsKeys.forEach(validatorKey => {
+                let inputValueToValidate = elements.contactDetailsForm.mainContent.querySelector(`#${validatorKey}`).value;
+                let validationRule = ValidationRulesForContactDetailsMap.get(validatorKey);
+                console.log(validationRule);
+                let response = Validator(validationRule, inputValueToValidate);
+                if (response.isValid) HideError(validatorKey)
+                else ShowError(validatorKey, response.errorMessage);
+
+                isAllInputsValid.push(response.isValid);
+            })
+            return isAllInputsValid.every(x => x); // Contact form Validation
+        // You can Add other validations here for other Forms :)
+        default :
+
+            break;
+    }
+    return true;
+}
+const ShowError = function (fieldName, errorMessage) {
+    let errorSpan = document.getElementsByClassName(`${fieldName}-error`)[0];
+    errorSpan.innerText = errorMessage;
+    errorSpan.classList.remove('hidden');
+}
+const HideError = function (fieldName) {
+    document.getElementsByClassName(`${fieldName}-error`)[0].classList.add('hidden');
+}
+const MoveToNextStep = function () {
+    let nextSteps = steps.filter((item, index) => index > currentStepIndex);
+    let completedSteps = steps.filter((item, index) => index < currentStepIndex);
+    let inProcessStep = steps.find((item, index) => index === currentStepIndex);
+
+    // Hide Current Form/Content
+    inProcessStep.progressBar.classList.add('completed');
+    inProcessStep.element.classList.add('hidden');
+
+    // Open Next Step/Form
+    if (!nextSteps.length) return;
+
+    let newCurrentForm = nextSteps.shift();
+    newCurrentForm.progressBar.classList.add('in-process');
+    newCurrentForm.element.classList.remove('hidden');
+
+    // Mark All Previus Steps as Completed
+    completedSteps.forEach(step=>{
+        step.progressBar.classList.remove('in-process');
+        step.element.classList.add('hidden');
+    })
+    currentStepIndex++;
+}
 // #region Details Form Events
-
+elements.contactDetailsForm.phone.addEventListener('keypress', (event) => {
+    if (!Number(event.key) || elements.contactDetailsForm.phone.value.length > 9)
+        event.preventDefault();
+});
 elements.home_details_elements.home_radio_buttons.forEach((element => {
     element.addEventListener('click', function (event) {
         elements.home_details_elements.home_radio_buttons.forEach((tempElement) => {
@@ -124,51 +183,13 @@ elements.home_details_elements.home_radio_buttons.forEach((element => {
         event.preventDefault();
     })
 }))
-
-
 elements.button_next_step.addEventListener('click', (event) => {
-    let response = IsFirstStepValid();
-    Object.assign(multiStepInfo,response.data);
+    let isCurrentFormValid = ValidateCurrentForm();
 
-    if(response.isValid){
-        console.log(multiStepInfo);
-        ShowErrors(false);
-    }
-    else{
-        ShowErrors(true);
-        return;
-    }
+    if (!isCurrentFormValid) return;
+    if (currentStepIndex === steps.length - 1) return;
 
+    MoveToNextStep();
 
-
-    if (currentStepIndex === steps.length - 1) {
-        alert('Last Step')
-        return;
-    }
-
-    let completedSteps = steps.filter((item,index)=>index < currentStepIndex);
-    let inProcessStep = steps.find((item,index)=>index === currentStepIndex);
-    let nextSteps = steps.filter((item,index)=>index > currentStepIndex);
-
-    completedSteps.forEach(element=>{
-       element.progressBar.classList.add('completed');
-       element.element.classList.add('hidden');
-    });
-    inProcessStep.progressBar.classList.add('in-process');
-    inProcessStep.element.classList.remove('hidden');
-
-    nextSteps.forEach(el=>{
-        el.element.classList.add('hidden');
-        el.progressBar.classList.remove('in-process');
-        el.progressBar.classList.remove('completed');
-    })
-    nextSteps.forEach(el=>{
-        el.element.classList.add('hidden');
-        el.progressBar.classList.remove('in-process');
-        el.progressBar.classList.remove('completed');
-    })
-
-    currentStepIndex++;
 })
-
 // #endregion
